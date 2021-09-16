76c9888222f88dd1e79f8b0d4609b3fc73764e81
siamshaeed <siamshaeed@gmail.com>
2021-09-16 16:29:55 +0600

    model link with controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,12 +15,7 @@ class CategoryController extends Controller
 
     public function newCategory(Request $request)
     {
-        // $category = new Category();
-        // $category->category_name         = $request->category_name;
-        // $category->category_description  = $request->category_description;
-        // $category->publication_status    = $request->publication_status;
-        // $category->save();
-        Category::create($request->all());
+        Category::saveCategoryInfo($request); //linke Category model
         return redirect('/category/add-category')->with('message', 'Category insert Successfully');
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,13 @@ class Category extends Model
 {
     use HasFactory;
     protected $fillable = ['category_name', 'category_description', 'publication_status'];
+
+    public static function saveCategoryInfo($request)   //This function for data save
+    {
+        $category = new Category();
+        $category->category_name         = $request->category_name;
+        $category->category_description  = $request->category_description;
+        $category->publication_status    = $request->publication_status;
+        $category->save();
+    }
 }
