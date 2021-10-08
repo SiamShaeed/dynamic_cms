464e1f41840f1b68108005eb08b70051f49dc2cc
siamshaeed <siamshaeed@gmail.com>
2021-10-08 22:46:31 +0600

    code formate

--- app/Models/Header.php
+++ app/Models/Header.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Header extends Model
 {
     use HasFactory;
-    protected $fillable =['header_title', 'header_content', 'header_image', 'publication_status'];
+    protected $fillable = ['header_title', 'header_content', 'header_image', 'publication_status'];
 }
